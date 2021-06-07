bcfa440460331168a6e173a21b453ee66abce024
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 08:26:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,5 +80,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday_name|
     holiday_name.each do |name, supply|
-      if supply.include? 
+      if supply.include?("BBQ")
+        
 end
